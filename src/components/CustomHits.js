ddbe2f811ad9ce950c5446eb12d577c1ed90c479
Pat Walsh <pmwals09@gmail.com>
2020-06-03 20:53:47 -0400

    Hover styling, filters

--- src/components/CustomHits.js
+++ src/components/CustomHits.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { connectHits } from 'react-instantsearch-dom'
+import _ from 'lodash'
 
 const Hits = ({ hits }) => {
   const legislatorList = hits.map(hit => {
+    let partyColor = _.lowerCase(hit.terms[hit.terms.length - 1].party[0])
     return (
-      <div className="cell small-12 medium-4 large-3 legislator">
+      <div className={`cell small-12 medium-4 large-3 legislator ${partyColor}`} key={hit.id.bioguide}>
         <h6>
           {hit.name.first} {hit.name.last} -{' '}
           {hit.terms[hit.terms.length - 1].party[0]} -{' '}
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import algoliasearch from 'algoliasearch/lite';
 import { InstantSearch, SearchBox, ClearRefinements, RefinementList } from 'react-instantsearch-dom';
+import { orderBy } from 'lodash'
 
 import CustomHits from '../components/CustomHits';
 
@@ -14,10 +15,24 @@ const SearchContainer = () => {
     <div>
       <InstantSearch searchClient={searchClient} indexName="dev_MINTBEAN">
         <div className="grid-x">
-          <div className="cell small-3">
+          <div className="cell small-2">
             <ClearRefinements />
             <h2>States</h2>
-            <RefinementList attribute="terms.state" />
+            <div className="refinement-list">
+              <RefinementList
+                attribute="terms.state"
+                limit={55}
+                transformItems={items => orderBy(items, "label", "asc")}
+              />
+            </div>
+            <h2>Party</h2>
+            <div className="refinement-list">
+              <RefinementList
+                attribute="terms.party"
+                limit={5}
+                transformItems={items => orderBy(items, "label", "asc")}
+              />
+            </div>
           </div>
           <div className="cell auto">
             <SearchBox />
